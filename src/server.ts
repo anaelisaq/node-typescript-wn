ce7807be609188e177c70c6b1a3ab7011e55d1e4
anaelisaq <anaelisa.abs@gmail.com>
2023-03-01 09:54:22 -0300

    last config setup: prettier

--- src/server.ts
+++ src/server.ts
@@ -4,28 +4,28 @@ import bodyParser from 'body-parser';
 import { ForecastController } from './controllers/forecast';
 import { Application } from 'express';
 
-export class SetupServer extends Server{
-    constructor(private port = 5000) {
-        super();
-    }
+export class SetupServer extends Server {
+  constructor(private port = 5000) {
+    super();
+  }
 
-    public init(): void {
-        this.setupExpress();
-        this.setupController();
-    }
+  public init(): void {
+    this.setupExpress();
+    this.setupController();
+  }
 
-    private setupExpress(): void {
-        this.app.use(bodyParser.json());
-    }
+  private setupExpress(): void {
+    this.app.use(bodyParser.json());
+  }
 
-    private setupController(): void {
-        const forecastController = new ForecastController();
-        this.addControllers([forecastController]);
-    }
+  private setupController(): void {
+    const forecastController = new ForecastController();
+    this.addControllers([forecastController]);
+  }
 
-    public getApp(): Application {
-        return this.app;
-    }
+  public getApp(): Application {
+    return this.app;
+  }
 }
 
 //seprar a inicialização permitindo quem inicie tenha controle de como ele vai ser inicializado
